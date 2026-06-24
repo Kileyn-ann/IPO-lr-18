cbe91297f1ec293ac486fcb1101ce945145ff6f8
Лиза <lizadan2008.s4@gmail.com>
2026-06-24 22:47:32 +0300

    Update forms.py

--- shop/forms.py
+++ shop/forms.py
@@ -1,1 +1,36 @@
+from django import forms  # базовый модуль форм Django
+
+from .models import Category, Manufacturer, Product
+
+
+class CategoryForm(forms.ModelForm):
+    """Форма для добавления категории товара."""
+
+    class Meta:
+        model = Category
+        fields = ["name", "description"]
+
+
+class ManufacturerForm(forms.ModelForm):
+    """Форма для добавления производителя."""
+
+    class Meta:
+        model = Manufacturer
+        fields = ["name", "country", "description"]
+
+
+class ProductForm(forms.ModelForm):
+    """Форма для добавления товара."""
+
+    class Meta:
+        model = Product
+        fields = [
+            "name",
+            "description",
+            "product_photo",
+            "price",
+            "stock_quantity",
+            "category",
+            "manufacturer",
+        ]
 
