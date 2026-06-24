834c0f0022084716122113cd3ac150e5d984ba19
Лиза <lizadan2008.s4@gmail.com>
2026-06-24 22:49:57 +0300

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,1 +1,22 @@
+from django.urls import path
+from .views import *
+from django.contrib.auth.views import LogoutView
+urlpatterns = [
+    path('cart/', cart_view, name='cart'),
+    path('catalog/', catalog, name='catalog'),
+    path('logout/', LogoutView.as_view(next_page='/admin/login/'), name='logout'),
+    path('', home, name='home'),
+    path('about_author/', about_author, name='about_author'),
+    path('about_shop/', about_shop, name='about_shop'),
+    path('add_category/', add_category, name='add_category'),
+    path('add_manufacturer/', add_manufacturer, name='add_manufacturer'),
+    path('add_product/', add_product, name='add_product'),
 
+
+    path('api/categories/', CategoryListAPIView.as_view()),
+    path('api/products/', ProductListAPIView.as_view()),
+    path('api/products/<int:pk>/', ProductDetailAPIView.as_view()),
+    path('api/products/create/', ProductCreateAPIView.as_view()),
+    path('api/products/<int:pk>/update/', ProductUpdateAPIView.as_view()),
+    path('api/products/<int:pk>/delete/', ProductDeleteAPIView.as_view()),
+]
