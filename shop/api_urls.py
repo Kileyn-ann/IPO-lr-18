7a17df9937c69fb218bd1c867ea9c3e83c7011c8
Лиза <lizadan2008.s4@gmail.com>
2026-06-24 22:45:33 +0300

    Update api_urls.py

--- shop/api_urls.py
+++ shop/api_urls.py
@@ -1,1 +1,16 @@
+from rest_framework.routers import DefaultRouter  # автоматическая генерация URL для ViewSet
 
+from .api_views import (
+    ProductViewSet,
+    CategoryViewSet,
+    ManufacturerViewSet,
+    CartItemViewSet,
+)
+
+router = DefaultRouter()
+router.register(r"products", ProductViewSet)
+router.register(r"categories", CategoryViewSet)
+router.register(r"manufacturers", ManufacturerViewSet)
+router.register(r"cart-items", CartItemViewSet)
+
+urlpatterns = router.urls
