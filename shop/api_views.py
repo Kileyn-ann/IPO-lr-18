dd651e64b5c3b5e705ccf1321e251b96cfaa6ba9
Лиза <lizadan2008.s4@gmail.com>
2026-06-24 22:46:23 +0300

    Update api_views.py

--- shop/api_views.py
+++ shop/api_views.py
@@ -1,1 +1,29 @@
+from rest_framework import viewsets
 
+from .models import Product, Category, Manufacturer, CartItem
+from .serializers import (
+    ProductSerializer,
+    CategorySerializer,
+    ManufacturerSerializer,
+    CartItemSerializer,
+)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ManufacturerViewSet(viewsets.ModelViewSet):
+    queryset = Manufacturer.objects.all()
+    serializer_class = ManufacturerSerializer
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
